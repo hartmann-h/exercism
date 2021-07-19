1086b721b587964c66e0d794702d8db7d4f3f3ad
hartmann-h <mr.hydrogen2@gmail.com>
2021-07-20 00:40:30 +0900

    first commit was wrooong

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,12 @@
 def convert(number):
-    pass
+    ans = ""
+    if number % 3 == 0:
+        ans += "Pling"
+    if number % 5 == 0:
+        ans += "Plang"
+    if number % 7 == 0:
+        ans += "Plong"
+    if not ans:
+        return str(number)
+    else:
+        return ans
